bf0f9cf1baacf633d125ed35e04295519e10d783
Matthew <matveynikishaev@gmail.com>
2024-01-03 23:34:47 +0300

    first routes filters impl

--- examTask/main.js
+++ examTask/main.js
@@ -1,4 +1,5 @@
 var routes;
+var filteredRoutes;
 var currentPage = 0;
 var routesPerPage = 3;
 
@@ -7,8 +8,10 @@ function fetchAndPopulateRoutes(apiUrl) {
         .then(response => response.json())
         .then(data => {
             routes = data;
+            filteredRoutes = [...routes];
             populateTable();
             setupPagination();
+            fillRouteFilterOptions();
         })
         .catch(error => console.error('Error fetching routes:', error));
 }
@@ -16,13 +19,16 @@ function fetchAndPopulateRoutes(apiUrl) {
 function populateTable() {
     const tableBody = document.querySelector('#routesTable tbody');
     tableBody.innerHTML = '';
-    for (var i = currentPage * routesPerPage; i < Math.min(routes.length, currentPage * routesPerPage + routesPerPage); i++) {
+
+    for (var i = currentPage * routesPerPage; i < Math.min(filteredRoutes.length, currentPage * routesPerPage + routesPerPage); i++) {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${routes[i].name}</td>
-            <td>${routes[i].description}</td>
-            <td>${routes[i].mainObject}</td>
-            <td><button class="route-select-button" data-route-id="${routes[i].id}">Выбрать</button></td>
+            <td class = 'text-center'>
+                <h5>${filteredRoutes[i].name}</h5>
+                <button class="btn btn-success btn-sm route-select-button" data-route-id="${routes[i].id}">Записаться</button>
+            </td>
+            <td>${filteredRoutes[i].description}</td>
+            <td>${filteredRoutes[i].mainObject}</td>
         `;
         tableBody.appendChild(row);
     }
@@ -39,25 +45,69 @@ function createPaginationButton(content) {
     li.classList.add('page-item');
     const a = document.createElement('a');
     a.classList.add('page-link');
+    a.href = '#pagination'
     a.innerHTML = content;
     li.appendChild(a);
     return li;
 }
 
+function fillRouteFilterOptions() {
+    var selectElement = document.querySelector('.form-select');
+    selectElement.innerHTML = '';
+    filteredRoutes.forEach(route => {
+        const names = extractNames(route.mainObject);
+        names.forEach(name => {
+            const option = document.createElement('option');
+            option.value = name;
+            option.text = name;
+            selectElement.appendChild(option);
+        });
+    });
+}
+
+function extractNames(inputString) {
+    const separators = ['-', '«', '»'];
+    const matches = [];
+    let startIndex = 0;
+
+    while (startIndex < inputString.length) {
+        let minIndex = inputString.length;
+
+        separators.forEach(separator => {
+            const index = inputString.indexOf(separator, startIndex);
+            if (index !== -1 && index < minIndex) {
+                minIndex = index;
+            }
+        });
+
+        if (minIndex < inputString.length) {
+            const name = inputString.slice(startIndex, minIndex).trim().substring(0, 25);
+            if (name.length > 0) {
+                matches.push(name);
+            }
+            startIndex = minIndex + 1;
+        } else {
+            break;
+        }
+    }
+
+    return matches;
+}
+
 function setupPagination() {
-    const totalPages = Math.ceil(routes.length / routesPerPage);
+    const totalPages = Math.ceil(filteredRoutes.length / routesPerPage);
 
     const pagination = document.querySelector('#pagination');
     pagination.innerHTML = '';
 
     var prev = createPaginationButton('<span aria-hidden="true">&laquo;</span>');
     prev.onclick = function () {
-        currentPage = Math.max(0,currentPage - 10);
+        currentPage = Math.max(0, currentPage - 7);
         setupPagination();
     };
     pagination.appendChild(prev);
 
-    for (let i = currentPage; i <= Math.min(totalPages - 1,currentPage + 10); i++) {
+    for (let i = currentPage; i <= Math.min(totalPages - 1, currentPage + 7); i++) {
         var item = createPaginationButton(i);
         item.onclick = function () {
             currentPage = i;
@@ -67,12 +117,42 @@ function setupPagination() {
     }
     var next = createPaginationButton('<span aria-hidden="true">&raquo;</span>');
     next.onclick = function () {
-            if (currentPage + 10 < totalPages - 1) {
-                currentPage = currentPage + 10;
-                setupPagination();
-            }
+        if (currentPage + 7 < totalPages - 1) {
+            currentPage = currentPage + 7;
+            setupPagination();
+        }
     };
     pagination.appendChild(next);
+}
+
+document.getElementById('searchIcon').addEventListener('click', () => {
+    const searchValue = document.getElementById('routes-search').value.toLowerCase().trim();
+    if (searchValue.length === 0) {
+        filteredRoutes = [...routes];
+    } else {
+        filteredRoutes = routes.filter(route => route.name.toLowerCase().includes(searchValue));
+        if (filteredRoutes.length === 0) {
+            showAlert("Извините, маршрутов с таким названием не найдено");
+        }
+    }
+    currentPage = 0;
+    populateTable();
+    setupPagination();
+});
+
+function showAlert(message) {
+    const alertContainer = document.querySelector('#notifications .container');
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert', 'alert-warning', 'alert-dismissible', 'm-0', 'my-2');
+    alertDiv.innerHTML = `
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    `;
+    alertContainer.appendChild(alertDiv);
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    setTimeout(() => {
+        alertDiv.remove();
+    }, 3000);
 }
 
 chooseRoute = function (routeId) {
@@ -84,3 +164,5 @@ window.onload = function () {
     apiUrl.searchParams.set('api_key', '99bc2746-b676-49f8-9383-984c72aa1141');
     fetchAndPopulateRoutes(apiUrl);
 };
+
+
